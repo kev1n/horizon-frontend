a408ce0ceed62a7fa7aac0997b7a1a195434c74a
Kevin Wang <kwang30@stuy.edu>
2023-10-14 23:13:02 -0400

    use siws phantom as template

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
-export { Loader } from "./Loader";
-export { SolanaLogo } from "./SolanaLogo";
-export { SelectAndConnectWalletButton } from "./SelectAndConnectWalletButton";
+export { default as Button } from './Button';
+export { default as Logs } from './Logs';
+export { default as NoProvider } from './NoProvider';
+export { default as Sidebar } from './Sidebar';
+export { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider';
